e92de05c903762e0baf561f5c5488136e8c7ae39
CoderUzair23 <uzairalisoomro112@gmail.com>
2026-02-12 17:25:08 +0000

    changes done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123123
+let accountEmail = "uzairsoomro73@gmail.com"
+var accountPassword = "123soomro"
+accountCity = "Larkana"
+let accountState;
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
