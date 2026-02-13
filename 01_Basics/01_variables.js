7cd162ad0481fbbe49328d6ecc781f2f2087e78b
CoderUzair23 <uzairalisoomro112@gmail.com>
2026-02-13 16:06:58 +0000

    Done

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,11 +1,20 @@
-const accountId = 123123
-let accountEmail = "uzairsoomro73@gmail.com"
-var accountPassword = "123soomro"
-accountCity = "Larkana"
-let accountState;
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
-*/
+// const accountId = 123123
+// let accountEmail = "uzairsoomro73@gmail.com"
+// var accountPassword = "123soomro"
+// accountCity = "Larkana"
+// let accountState;
+// console.log(accountId)
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
 
-console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+// console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
+let usersData = 20021
+const selaryAmount = 40000
+// selaryAmount = 600000
+let emopleyNames = ["Uzair","Ali","Hamza"]
+console.table([usersData,selaryAmount,emopleyNames])
