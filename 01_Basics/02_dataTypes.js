75afdae0e82b7177382ae9b1ddb13dc654749337
CoderUzair23 <uzairalisoomro112@gmail.com>
2026-02-13 16:32:50 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Uzair"
+let age = 21
+let state;
+
+
+console.log(typeof name)
+
+// Number -> 2 to power 53
+// bigInt
+// string -> ""
+//boolean = true/false
+// null -> standalone value
+// undefined ->
+// symbol -> unique
+
+
+// object
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
